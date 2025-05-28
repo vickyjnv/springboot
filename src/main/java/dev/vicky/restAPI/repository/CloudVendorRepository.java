2447db53be8d687ca2d3905d65338c4133dd097b
Vicky <vickyjnv7@gmail.com>
2025-05-28 22:06:49 +0530

    testing

--- src/main/java/dev/vicky/restAPI/repository/CloudVendorRepository.java
+++ src/main/java/dev/vicky/restAPI/repository/CloudVendorRepository.java
@@ -3,5 +3,8 @@ package dev.vicky.restAPI.repository;
 import dev.vicky.restAPI.Model.CloudVendor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CloudVendorRepository extends JpaRepository<CloudVendor,String> {
+  List<CloudVendor> findByVendorName(String vendorName);
 }
--- src/test/java/dev/vicky/restAPI/repository/CloudVendorRepositoryTest.java
+++ src/test/java/dev/vicky/restAPI/repository/CloudVendorRepositoryTest.java
@@ -1,4 +1,57 @@
 package dev.vicky.restAPI.repository;
 
+import dev.vicky.restAPI.Model.CloudVendor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
 public class CloudVendorRepositoryTest {
+
+
+  @Autowired
+  private CloudVendorRepository cloudVendorRepository;
+  CloudVendor cloudVendor;
+
+  @BeforeEach
+  void setUp() {
+    cloudVendor=new CloudVendor("1","Amazon","USA","9784");
+    cloudVendorRepository.save(cloudVendor);
+  }
+
+  @AfterEach
+  void tearDown() {
+    cloudVendor=null;
+    cloudVendorRepository.deleteAll();
+  }
+
+  //Test case sucess
+  @Test
+  void testFindByVendorName_Found()
+  {
+    List<CloudVendor> cloudVendorList=cloudVendorRepository.findByVendorName("Amazon");
+    assertThat(cloudVendorList.get(0).getVendorId()).isEqualTo(cloudVendor.getVendorId());
+    assertThat(cloudVendorList.get(0).getVendorAddress()).isEqualTo(cloudVendor.getVendorAddress());
+  }
+
+
+  //Test case fail
+  @Test
+  void testFindByVendorName_NotFound()
+  {
+    List<CloudVendor> cloudVendorList=cloudVendorRepository.findByVendorName("GCP");
+    assertThat(cloudVendorList.isEmpty()).isFalse();
+    assertThat(cloudVendorList.isEmpty()).isTrue();
+  }
+
+
+
 }
+
+
